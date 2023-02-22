a369b224ddd6c11723af9052d13626bb2aa5a25b
ctrlouis <lo.bertschy@gmail.com>
2023-02-22 23:53:10 +0100

    Display sticker with url stickers/<stickerName>

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(morgan('dev'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send("Welcome to html2stickers! 📄");
+});
+
+router.get('/stickers/:stickerName', function(req, res, next) {
+    const stickerName = req.params.stickerName;
+    const height = 306;
+    const width = 991;
+    const data = {
+        title: stickerName,
+        height: height,
+        width: width,
+    }
+    res.render(stickerName, data);
 });
 
 export default router;
